70f280bd9141b5080c6cdd1037c5c43d2705e157
Ding <Ding@Ding.com>
2022-09-08 12:33:51 +0800

    add deleting animation

--- client/src/components/Lists.js
+++ client/src/components/Lists.js
@@ -1,25 +1,39 @@
 import React, { useEffect } from "react";
-import SingleList from "./SingleList";
+import CRUDService from "../services/crud";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const Lists = ({ currentLists, setCurrentLists }) => {
   useEffect(() => {
     setCurrentLists(JSON.parse(localStorage.getItem("user")).lists);
   }, []);
 
+  const removeHandler = (id) => {
+    CRUDService.removeSubject(id)
+      .then((res) => {
+        localStorage.setItem("user", JSON.stringify(res.data));
+        setCurrentLists(JSON.parse(localStorage.getItem("user")).lists);
+      })
+      .catch((err) => {
+        alert(err.response.data);
+      });
+  };
+
   return (
-    <div className="lists">
-      <ul>
-        {currentLists.map((list) => {
-          return (
-            <SingleList
-              subject={list.subject}
-              id={list._id}
-              setCurrentLists={setCurrentLists}
-            />
-          );
-        })}
-      </ul>
-    </div>
+    <TransitionGroup className="lists">
+      {currentLists.map((list) => (
+        <CSSTransition timeout={500} key={list._id} classNames="fade">
+          <div className="singleList">
+            <li>{list.subject}</li>
+            <i
+              class="fa-solid fa-trash-can remove"
+              onClick={() => {
+                removeHandler(list._id);
+              }}
+            ></i>
+          </div>
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
   );
 };
 
